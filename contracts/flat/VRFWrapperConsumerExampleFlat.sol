c03bc42bddeeffa421cfa737d462010c7d8f8caa
Lin <linyaping@linyapingdeMacBook-Pro.local>
2023-03-03 12:23:16 +0800

    add flat contract for toturial

---
+++ contracts/flat/VRFWrapperConsumerExampleFlat.sol
@@ -0,0 +1,337 @@
+// File: @openzeppelin/contracts/utils/Context.sol
+
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+// File: @openzeppelin/contracts/access/Ownable.sol
+
+// OpenZeppelin Contracts (last updated v4.7.0) (access/Ownable.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(
+            newOwner != address(0),
+            "Ownable: new owner is the zero address"
+        );
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+// File: contracts/interfaces/VRFV2WrapperInterface.sol
+
+pragma solidity ^0.8.7;
+
+interface VRFV2WrapperInterface {
+    /**
+     * @return the request ID of the most recent VRF V2 request made by this wrapper. This should only
+     * be relied option within the same transaction that the request was made.
+     */
+    function lastRequestId() external view returns (uint256);
+
+    /**
+     * @notice Calculates the price of a VRF request with the given callbackGasLimit at the current
+     * @notice block.
+     *
+     * @dev This function relies on the transaction gas price which is not automatically set during
+     * @dev simulation. To estimate the price at a specific gas price, use the estimatePrice function.
+     *
+     * @param _callbackGasLimit is the gas limit used to estimate the price.
+     */
+    function calculateRequestPrice(uint32 _callbackGasLimit)
+        external
+        view
+        returns (uint256);
+
+    /**
+     * @notice Estimates the price of a VRF request with a specific gas limit and gas price.
+     *
+     * @dev This is a convenience function that can be called in simulation to better understand
+     * @dev pricing.
+     *
+     * @param _callbackGasLimit is the gas limit used to estimate the price.
+     * @param _requestGasPriceWei is the gas price in wei used for the estimation.
+     */
+    function estimateRequestPrice(
+        uint32 _callbackGasLimit,
+        uint256 _requestGasPriceWei
+    ) external view returns (uint256);
+
+    /**
+     * @notice Consumer contract call this function with OKT to requestRandomWords.
+     *
+     * @dev This function receives the OKT sent by the user and calls the VRFCoordinatorV2 contract
+     * @dev to request a random number.
+     *
+     * @param _sender is the target for fulfillRandomWords.
+     * @param _amount is the OKT amount consumer contract send to this contract.
+     * @param _data is the abi encode for
+     *      uint32 callbackGasLimit,
+     *      uint16 requestConfirmations,
+     *      uint32 numWords
+     */
+
+    function charge(
+        address _sender,
+        uint256 _amount,
+        bytes calldata _data
+    ) external payable;
+}
+
+// File: contracts/interfaces/VRFV2WrapperConsumerBase.sol
+
+pragma solidity ^0.8.0;
+
+/** *******************************************************************************
+ * @notice Interface for contracts using VRF randomness through the VRF V2 wrapper
+ * ********************************************************************************
+ * @dev PURPOSE
+ *
+ * @dev Create VRF V2 requests without the need for subscription management. Rather than creating
+ * @dev and funding a VRF V2 subscription, a user can use this wrapper to create one off requests,
+ * @dev paying up front rather than at fulfillment.
+ *
+ * @dev Since the price is determined using the gas price of the request transaction rather than
+ * @dev the fulfillment transaction, the wrapper charges an additional premium on callback gas
+ * @dev usage, in addition to some extra overhead costs associated with the VRFV2Wrapper contract.
+ * *****************************************************************************
+ * @dev USAGE
+ *
+ * @dev Calling contracts must inherit from VRFV2WrapperConsumerBase. The consumer must be funded
+ * @dev with enough OKT to make the request, otherwise requests will revert. To request randomness,
+ * @dev call the 'requestRandomness' function with the desired VRF parameters. This function handles
+ * @dev paying for the request based on the current pricing.
+ *
+ * @dev Consumers must implement the fullfillRandomWords function, which will be called during
+ * @dev fulfillment with the randomness result.
+ */
+abstract contract VRFV2WrapperConsumerBase is Ownable {
+    VRFV2WrapperInterface public immutable VRF_V2_WRAPPER;
+    event RequestSent(uint256 requestId, uint32 numWords);
+
+    struct RequestStatus {
+        uint256 paid; // amount paid in OKT
+        bool fulfilled; // whether the request has been successfully fulfilled
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus) public s_requests; /* requestId --> requestStatus */
+
+    // past requests Id.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    /**
+     * @param _vrfV2Wrapper is the address of the VRFV2Wrapper contract
+     */
+
+    constructor(address _vrfV2Wrapper) Ownable() {
+        VRF_V2_WRAPPER = VRFV2WrapperInterface(_vrfV2Wrapper);
+    }
+
+    function getRequestStatus(uint256 _requestId)
+        external
+        view
+        returns (
+            uint256 paid,
+            bool fulfilled,
+            uint256[] memory randomWords
+        )
+    {
+        require(s_requests[_requestId].paid > 0, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.paid, request.fulfilled, request.randomWords);
+    }
+
+    /**
+     * @dev Requests randomness from the VRF V2 wrapper.
+     *
+     * @param _callbackGasLimit is the gas limit that should be used when calling the consumer's
+     *        fulfillRandomWords function.
+     * @param _requestConfirmations is the number of confirmations to wait before fulfilling the
+     *        request. A higher number of confirmations increases security by reducing the likelihood
+     *        that a chain re-org changes a published randomness outcome.
+     * @param _numWords is the number of random words to request.
+     *
+     * @return requestId is the VRF V2 request ID of the newly created randomness request.
+     */
+    function requestRandomness(
+        uint32 _callbackGasLimit,
+        uint16 _requestConfirmations,
+        uint32 _numWords
+    ) external payable virtual returns (uint256 requestId) {
+        uint256 requestPrice = VRF_V2_WRAPPER.calculateRequestPrice(
+            _callbackGasLimit
+        );
+        VRF_V2_WRAPPER.charge{value: msg.value}(
+            address(this),
+            requestPrice,
+            abi.encode(_callbackGasLimit, _requestConfirmations, _numWords)
+        );
+        lastRequestId = VRF_V2_WRAPPER.lastRequestId();
+        requestIds.push(lastRequestId);
+        s_requests[lastRequestId].paid = requestPrice;
+        return lastRequestId;
+    }
+
+    /**
+     * @notice withdraw is used by owner to withdraw OKT revenue.
+     */
+    function withdraw() external onlyOwner {
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance,
+            gas: 8000
+        }("");
+        require(success, "VRFV2Wrapper::sendOKT: transfer OKT failed");
+    }
+
+    /**
+     * @notice fulfillRandomWords handles the VRF V2 wrapper response. The consuming contract must
+     * @notice implement it.
+     *
+     * @param _requestId is the VRF V2 request ID.
+     * @param _randomWords is the randomness result.
+     */
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal virtual;
+
+    function rawFulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) external {
+        require(
+            msg.sender == address(VRF_V2_WRAPPER),
+            "only VRF V2 wrapper can fulfill"
+        );
+        fulfillRandomWords(_requestId, _randomWords);
+    }
+}
+
+// File: contracts/VRFWrapperConsumerExample.sol
+
+// An example of a consumer contract that directly pays for each request.
+pragma solidity ^0.8.7;
+
+contract VRFV2WrapperConsumerExample is VRFV2WrapperConsumerBase {
+    event RequestFulfilled(
+        uint256 requestId,
+        uint256[] randomWords,
+        uint256 payment
+    );
+
+    constructor(address _vrfV2Wrapper)
+        VRFV2WrapperConsumerBase(_vrfV2Wrapper)
+    {}
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(s_requests[_requestId].paid > 0, "request not found");
+
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(
+            _requestId,
+            _randomWords,
+            s_requests[_requestId].paid
+        );
+    }
+}
